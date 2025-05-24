9d7eb9d9d74399f8e3ae690431598a8ec5880207
MariamTimite <timitemariam789@gmail.com>
2025-05-24 13:00:07 +0000

    update

---
+++ email-sender.js
@@ -0,0 +1,27 @@
+const nodemailer = require("nodemailer");
+
+// Configurer le transporteur (avec vos informations d'identification)
+const transporter = nodemailer.createTransport({
+  service: "gmail",
+  auth: {
+    user: "timitemariam789@gmail.com",
+    pass: "Timite789@",
+  },
+});
+
+// Définir les options de l'e-mail
+const mailOptions = {
+  from: "timitemariam789@gmail.com",
+  to: "matimite.digifemmes@gmail.com",
+  subject: "Test NodeMailer",
+  text: "Hello from Node.js!",
+};
+
+// Envoyer l'e-mail
+transporter.sendMail(mailOptions, (err, info) => {
+  if (err) {
+    console.log("Echec d'envoie d'email");
+  } else {
+    console.log("Email envoyé:", info.response);
+  }
+});
